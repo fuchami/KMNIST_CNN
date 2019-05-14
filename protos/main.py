41aa86e51efa2c0d6e1674d0719cee3e783b2853
fuchami <famichiki.yuuki@gmail.com>
2019-05-14 16:07:33 +0900

    ループスクリプト

---
+++ protos/main.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+
+import os, sys
+import subprocess
+
+def main():
+    cnn_model = ['prot3', 'resnet', 'prot2']
+    opt = ['adaboud','sgd','rmsprop']
+    z_score = ['True', 'False']
+    batchsize = ['256', '128', '64', '512']
+
+    for z in z_score:
+        for o in opt:
+            for b in batchsize:
+                for m in cnn_model:
+                    cmd = ["python3", 'train.py', "-m", m, "-b", b, "-o", o, "-z", z]
+                    print(cmd)
+                    result = subprocess.check_output(cmd)
+                    print(result)
+
+if __name__ == "__main__":
+    main()
