c51f28cda51614fe8b6a2c21790b486324627ea5
fuchami <famichiki.yuuki@gmail.com>
2019-05-24 17:38:47 +0000

    bug fixxxx

--- protos/model.py
+++ protos/model.py
@@ -28,47 +28,47 @@ def prot3_SE(args):
     x = Conv2D(32, (3,3), padding='same', kernel_initializer='he_normal',
                 kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(inputs)
     x = Activation('elu')(x)
-    x = BatchNormalization(momdentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
     if args.se : x = se_block(x, 32)
 
     x = Conv2D(32, (3,3), padding='same', kernel_initializer='he_normal',
-                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(inputs)
+                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(x)
     x = Activation('elu')(x)
-    x = BatchNormalization(momdentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
     x = MaxPooling2D(pool_size=(2,2))(x)
     x = Dropout(0.2)(x)
     if args.se : x = se_block(x, 32)
 
     x = Conv2D(64, (3,3), padding='same', kernel_initializer='he_normal',
-                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(inputs)
+                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(x)
     x = Activation('elu')(x)
-    x = BatchNormalization(momdentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
     if args.se : x = se_block(x, 64)
 
     x = Conv2D(64, (3,3), padding='same', kernel_initializer='he_normal',
-                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(inputs)
+                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(x)
     x = Activation('elu')(x)
-    x = BatchNormalization(momdentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
     x = MaxPooling2D(pool_size=(2,2))(x)
     x = Dropout(0.3)(x)
     if args.se : x = se_block(x, 64)
 
     x = Conv2D(128, (3,3), padding='same', kernel_initializer='he_normal',
-                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(inputs)
+                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(x)
     x = Activation('elu')(x)
-    x = BatchNormalization(momdentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
     if args.se : x = se_block(x, 128)
 
     x = Conv2D(128, (3,3), padding='same', kernel_initializer='he_normal',
-                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(inputs)
+                kernel_regularizer=regularizers.l2(weight_decay), use_bias=False)(x)
     x = Activation('elu')(x)
-    x = BatchNormalization(momdentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
     x = MaxPooling2D(pool_size=(2,2))(x)
     x = Dropout(0.4)(x)
     if args.se : x = se_block(x, 128)
 
     x = GlobalAveragePooling2D()(x)
-    x = Dense(10, activation='softmax')
+    x = Dense(10, activation='softmax')(x)
 
     model = Model(inputs, x)
     return model
